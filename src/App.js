f02c647b2d79a9a36d38c6a0823c06461bd36df3
alwaysInControl <127772513+alwaysInControl@users.noreply.github.com>
2023-06-24 17:17:04 +0600

    react hooks, useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Hooks_Practice() {
+    const[count, setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`
+    })
+
+    return (
+        <div>
+            <p>Number of clicks: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Click here</button>
+            <br/>
+            <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    )
 }
-
-export default App;
